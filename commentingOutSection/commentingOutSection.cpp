fa02626f3a6dcd2ebb8b4fccf223f3bce7991aa7
divineudoh123 <udohdivine123@gmail.com>
2026-07-01 18:53:43 +0100

    comment out practice

---
+++ commentingOutSection/commentingOutSection.cpp
@@ -0,0 +1,13 @@
+// commentingOutSection.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    /*
+    std::cout << "Hello World!\n";
+    std::cout << "I wanna edit out this part";
+    */
+    std::cout << "If i do not see it then it means i was succesful";
+}
